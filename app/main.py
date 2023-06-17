72a71bad94769e3e609726e597df9158b9fe36ec
Makani-C <61200430+Makani-C@users.noreply.github.com>
2023-06-17 12:44:12 -0700

    updates

--- app/main.py
+++ app/main.py
@@ -3,8 +3,7 @@ from datetime import datetime
 from configparser import ConfigParser
 from pydantic import BaseModel
 from typing import Optional
-
-from database_connection import PostgresRDS, connection_required
+from database_connection import PostgresRDS
 
 # Create a FastAPI app
 app = FastAPI()
@@ -12,6 +11,15 @@ app = FastAPI()
 # Read the database credentials from config.ini
 config = ConfigParser()
 config.read("config.ini")
+
+# Initialize the database connector
+db_connector = PostgresRDS(
+    host=config.get("rds", "rds_host"),
+    port=config.getint("rds", "rds_port"),
+    user=config.get("rds", "rds_user"),
+    password=config.get("rds", "rds_password"),
+    database=config.get("rds", "rds_database")
+)
 
 
 # Pydantic model for the Bill object
@@ -27,25 +35,9 @@ class Bill(BaseModel):
     updated_at: datetime
 
 
-class FilterConfig(BaseModel):
-    field: str
-    type: str
-    value: str
-
-
-# Initialize the database connector
-db_connector = PostgresRDS(
-    host=config.get("rds", "rds_host"),
-    port=config.get("rds", "rds_port"),
-    user=config.get("rds", "rds_user"),
-    password=config.get("rds", "rds_password"),
-    database=config.get("rds", "rds_database")
-)
-
-
 # Define a route to get all bills
 @app.get("/bills")
-def get_bills(bill_id: int = None, limit: int = 10, include_full_text: bool = False):
+def get_bills(bill_id: Optional[int] = None, limit: int = 10, include_full_text: bool = False):
     columns = [
         "bill_id", "state_code", "session_id", "body_id", "status_id",
         "pdf_link", "summary_text", "updated_at"
@@ -54,17 +46,14 @@ def get_bills(bill_id: int = None, limit: int = 10, include_full_text: bool = Fa
         columns.append("text")
 
     select_clause = f"SELECT {', '.join(columns)} FROM bills"
-    filter_clause = ""
-    limit_clause = ""
-
-    if bill_id:
-        filter_clause = f"WHERE bill_id = {bill_id}"
-    if limit:
-        limit_clause = f"LIMIT {limit}"
+    filter_clause = f"WHERE bill_id = :bill_id" if bill_id else ""
+    limit_clause = f"LIMIT :limit" if limit else ""
 
     query = f"{select_clause} {filter_clause} {limit_clause}"
 
-    rows = db_connector.execute_query(query)
+    params = {"bill_id": bill_id, "limit": limit}
+
+    rows = db_connector.execute_query(query, params)
     bills = [Bill(**row) for row in rows]
 
     return bills
