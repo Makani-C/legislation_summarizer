f2b0f5b9eac2fdf4c50289b7cd039b901fd893e4
Makani-C <61200430+Makani-C@users.noreply.github.com>
2023-06-17 12:29:57 -0700

    app functionality

--- app/main.py
+++ app/main.py
@@ -27,6 +27,12 @@ class Bill(BaseModel):
     updated_at: datetime
 
 
+class FilterConfig(BaseModel):
+    field: str
+    type: str
+    value: str
+
+
 # Initialize the database connector
 db_connector = PostgresRDS(
     host=config.get("rds", "rds_host"),
@@ -39,14 +45,25 @@ db_connector = PostgresRDS(
 
 # Define a route to get all bills
 @app.get("/bills")
-def get_bills(limit: int = 10, include_full_text: bool = False):
+def get_bills(bill_id: int = None, limit: int = 10, include_full_text: bool = False):
     columns = [
         "bill_id", "state_code", "session_id", "body_id", "status_id",
         "pdf_link", "summary_text", "updated_at"
     ]
     if include_full_text:
         columns.append("text")
-    query = f"SELECT {', '.join(columns)} FROM bills LIMIT {limit};"
+
+    select_clause = f"SELECT {', '.join(columns)} FROM bills"
+    filter_clause = ""
+    limit_clause = ""
+
+    if bill_id:
+        filter_clause = f"WHERE bill_id = {bill_id}"
+    if limit:
+        limit_clause = f"LIMIT {limit}"
+
+    query = f"{select_clause} {filter_clause} {limit_clause}"
+
     rows = db_connector.execute_query(query)
     bills = [Bill(**row) for row in rows]
 
