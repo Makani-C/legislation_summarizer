9b4fdf4b46d576c9d0d2368fc66940598bcea039
henrydalton <henry.r.dalton@gmail.com>
2023-08-05 14:39:24 -0700

    new requirements

--- pipeline/summarizer.py
+++ pipeline/summarizer.py
@@ -2,3 +2,20 @@ def summarize_text(text: str) -> str:
     return ""
 
 # this is my commment
+from datetime import datetime
+import pandas as pd
+import numpy as np
+import json
+import os
+import matplotlib.pyplot as plt
+from scipy.spatial.distance import cosine
+import networkx as nx
+from networkx.algorithms import community
+
+from langchain import OpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains.llm import LLMChain
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.docstore.document import Document
+from langchain.chains.summarize import load_summarize_chain
+
